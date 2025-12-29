1f3a5386d003428dcf53c3172fe5bb49eab1fa20
Saad Nadeem <saad29nadeem@gmail.com>
2025-12-29 16:21:25 -0500

    impala: 0.6.0 -> 0.7.0

--- pkgs/by-name/im/impala/package.nix
+++ pkgs/by-name/im/impala/package.nix
@@ -5,16 +5,16 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "impala";
-  version = "0.6.0";
+  version = "0.7.0";
 
   src = fetchFromGitHub {
     owner = "pythops";
     repo = "impala";
     rev = "v${version}";
-    hash = "sha256-FU/8g2zTTHm3Sdbxt9761Z+a0zaJMdAMdHrJIwjUrYs=";
+    hash = "sha256-lX6f81VMFCf3HhpHETe2vFfXScR5HoNe9TyCKZw0ylI=";
   };
 
-  cargoHash = "sha256-dpTLVlDxc9eK7GwbweODoJlrBZeYwVcv1fQ2UtYbg7k=";
+  cargoHash = "sha256-2a7obW93uzdA03GFRdLvMqCcRuRl7YPJHv4Geu5EvH0=";
 
   meta = {
     description = "TUI for managing wifi";
